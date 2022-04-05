511f53a0929012d5c2afa0104f660c4677fc5353
WheneverDev <whenever.fansub@gmail.com>
2022-04-05 18:16:59 +0200

    clean code and change button function

--- casquad/www/js/index.js
+++ casquad/www/js/index.js
@@ -11,13 +11,14 @@ var app = {
         const analog_characteristic_uuid = "ee04e831-6709-4f42-af61-3e66c48918b3";
         const channel_characteristic_uuid = "ee04e832-6709-4f42-af61-3e66c48918b3";
 
-        let device_array = [];
-        let scan_result = "";
         let message = document.getElementById('message');
-        let analogValue = document.getElementById('analogValue');
         let ledButton = document.getElementById('ledButton');
 
         console.log(navigator.vibrate);
+        console.log(StatusBar);
+
+        StatusBar.backgroundColorByName("white");
+        StatusBar.styleDefault();
 
         if (device_id === 0) {
             ble.scan([],120,scanSuccess,scanFailure);
@@ -28,10 +29,10 @@ var app = {
         }
 
         function scanSuccess(result) {
-            device_array.push(result)
-            scan_result += "</br>"+result.name;
-            scan_result += "</br>"+result.id;
-            message.innerHTML = scan_result;
+            // device_array.push(result)
+            // scan_result += "</br>"+result.name;
+            // scan_result += "</br>"+result.id;
+            // message.innerHTML = scan_result;
             if (result.name === service_name) {
                 device_id = result.id;
                 scanStop();
@@ -71,32 +72,26 @@ var app = {
         $("#channelButton").bind("click",changeChannel);
 
         function changeLED(){
+            navigator.vibrate(1000);
             let data = new Uint8Array(1);
             data[0] = 4;
             ble.write(device_id, service_uuid, channel_characteristic_uuid, data.buffer, ledSuccess, ledError);
         }
 
         function ledSuccess(){
-            ledButton.style.backgroundColor = "#29ff16ff"
+            let oldColor = ledButton.style.backgroundColor;
+            ledButton.style.backgroundColor = "#107e00"
+            setTimeout(() => {
+                ledButton.style.backgroundColor = oldColor;
+            }, 2000)
         }
 
         function ledError(){
-            ledButton.style.backgroundColor = "#ff0700ff"
-
-        }
-
-        function changeChannel() {
-            let data = new Uint8Array(1);
-            data[0] = $("#channel").val();
-            ble.write(device_id,service_uuid,channel_characteristic_uuid,data.buffer,changeChannel_success,changeChannel_error);
-        }
-
-        function changeChannel_success() {
-            message.innerHTML = "change channel success";
-        }
-
-        function changeChannel_error() {
-            message.innerHTML = "change channel error";
+            let oldColor = ledButton.style.backgroundColor;
+            ledButton.style.backgroundColor = "#8c0500";
+            setTimeout(() => {
+                ledButton.style.backgroundColor = oldColor;
+            }, 2000)
         }
     }
 };
